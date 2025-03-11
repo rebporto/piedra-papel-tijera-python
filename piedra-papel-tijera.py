66e6038ae432daff17789c515c4c525c34eefee9
Renata <renataporto@me.com>
2025-03-11 15:49:24 -0300

    exercicios

--- piedra-papel-tijera.py
+++ piedra-papel-tijera.py
@@ -1,27 +1,18 @@
-nombre1 = input("Como se llama jugador 1?: ")
-nombre2 = input("Como se llama jugador 2?: ")
+print(
+    "---------------------------numero par o impar -------------------------------------"
+)
+number = input("choose a number from 0 to 1000: ")
+number = int(number)
+if number % 2 == 0:
+    print("seu numero es par")
+else:
+    print("seu numero é impar")
 
-adivinado = False
+print(
+    "---------------------------contador de 1 ao 10 -------------------------------------"
+)
 
-
-while not adivinado:
-    jugador1 = input(f"Hola {nombre1}, qué eliges? Piedra, papel o tijera?: ")
-    jugador2 = input(f"Hola {nombre2}, qué eliges? Piedra, papel o tijera?: ")
-
-    if jugador1 == jugador2:
-        print("ha sido un empate!")
-
-    elif (
-        (jugador1 == "piedra" and jugador2 == "tijeras")
-        or (jugador1 == "papel" and jugador2 == "piedra")
-        or (jugador1 == "tijera" and jugador2 == "papel")
-    ):
-        print(f"ha ganado {nombre1}")
-    else:
-        print(f"ha ganado el {nombre2}")
-
-        jugar_de_nuevo = input("¿Quieren jugar otra vez? (sí/no): ").lower()
-        if jugar_de_nuevo != "sí":
-            adivinado = True  # Finaliza el bucle
-
-print("fin de juego")
+contador = 0
+while contador <= 10:
+    contador += 1
+    print("el contador es:", contador)
